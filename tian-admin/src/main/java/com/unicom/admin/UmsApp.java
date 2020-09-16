b5587566179726ce638f29c0a633cf6fa16667b6
caitf3 <1075079197@qq.com>
2020-09-17 07:47:31 +0800

    整合oauth2

--- tian-admin/src/main/java/com/unicom/admin/UmsApp.java
+++ tian-admin/src/main/java/com/unicom/admin/UmsApp.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"com.unicom.common","com.unicom.admin"})
+//@SpringBootApplication(scanBasePackages = {"com.unicom.common","com.unicom.admin"})
 @EnableFeignClients
 @EnableDiscoveryClient
-//@SpringBootApplication(scanBasePackages = "com.unicom")
+@SpringBootApplication(scanBasePackages = "com.unicom")
 public class UmsApp {
     public static void main(String[] args) {
         SpringApplication.run(UmsApp.class,args);
--- tian-admin/src/main/java/com/unicom/admin/config/MybatisPlusConfig.java
+++ tian-admin/src/main/java/com/unicom/admin/config/MybatisPlusConfig.java
@@ -8,9 +8,7 @@
 package com.unicom.admin.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import com.unicom.admin.datascope.DataScopeInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -31,15 +29,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MybatisPlusConfig {
 
   /** mybatis-plus SQL执行效率插件【生产环境可以关闭】 */
-  @Bean
+ /* @Bean
   public PerformanceInterceptor performanceInterceptor() {
     PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-    /*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*/
+    *//*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*//*
     performanceInterceptor.setMaxTime(1000);
-    /*<!--SQL是否格式化 默认false-->*/
+    *//*<!--SQL是否格式化 默认false-->*//*
     performanceInterceptor.setFormat(true);
     return performanceInterceptor;
-  }
+  }*/
 
   /**
    * mybatis-plus分页插件<br>
@@ -53,10 +51,10 @@ public class MybatisPlusConfig {
   }
 
   /** 注入sql注入器 */
-  @Bean
+  /*@Bean
   public ISqlInjector sqlInjector() {
     return new LogicSqlInjector();
-  }
+  }*/
 
   @Bean
   public DataScopeInterceptor mybatisSqlInterceptor() {
